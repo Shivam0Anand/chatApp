b3694bd0dd8cc38be83fd7dcad41459b035b9bf5
Shivam0Anand <shivam_anand@outlook.com>
2019-11-05 12:10:03 +0530

    index.html

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+app.listen(port, () => {
+  console.log(`Server is on Port ${port}!`);
+});

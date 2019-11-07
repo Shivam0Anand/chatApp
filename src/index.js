95cc6b45932d7f7f4696b4b0fa27cd3942b1dbf6
Shivam0Anand <shivam_anand@outlook.com>
2019-11-07 16:53:56 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -11,8 +11,19 @@ const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
-io.on("connection", () => {
+
+let count = 0;
+
+io.on("connection", socket => {
   console.log("new websoket connection");
+
+  socket.emit("countUpdated", count);
+
+  socket.on("increment", () => {
+    count++;
+    //socket.emit("countUpdated", count);
+    io.emit("countUpdated", count);
+  });
 });
 
 server.listen(port, () => {

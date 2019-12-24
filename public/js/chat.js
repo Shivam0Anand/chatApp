efa42b41328f728491e2ff9acf4ce8be46fe81cb
Shivam0Anand <shivam_anand@outlook.com>
2019-12-25 00:01:08 +0530

    fix in location butt

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,6 +4,7 @@ const socket = io();
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector("#send-location")
 
 socket.on("message", message => {
   console.log(message);
@@ -31,10 +32,12 @@ $messageForm.addEventListener("submit", e => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser!");
   }
+
+  $sendLocationButton.setAttribute('disabled', 'disabled')
 
   navigator.geolocation.getCurrentPosition(position => {
     // console.log(position);
@@ -42,6 +45,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     }, ()=>{
+      $sendLocationButton.removeAttribute('disabled')
       console.log("Location Shared!")
     });
   });

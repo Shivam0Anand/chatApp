b89a1b7ff56a1d4079e487cae9b518fe9bed5622
Shivam0Anand <shivam_anand@outlook.com>
2020-01-06 23:06:42 +0530

    modified

---
+++ src/utils/users.js
@@ -0,0 +1,13 @@
+const users = []
+
+const addUser = ({
+    id,
+    username,
+    room
+}) => {
+    // clean data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // validate data
+}

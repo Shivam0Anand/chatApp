658c6f7349cd6a2da1fc2c32c01863b2e18ecf56
Shivam0Anand <shivam_anand@outlook.com>
2020-01-06 23:11:55 +0530

    modified

--- src/utils/users.js
+++ src/utils/users.js
@@ -10,4 +10,32 @@ const addUser = ({
     room = room.trim().toLowerCase()
 
     // validate data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && room.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username already taken!'
+        }
+    }
+
+    // Store User
+    const user = {
+        id,
+        username,
+        room
+    }
+    user.push(user)
+    return {
+        user
+    }
 }
